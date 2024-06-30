2e9eb8a1098ba7232ee75cf41977b1d6ea4b1fd4
Smriti Panta <smritipanta1@gmail.com>
2024-06-30 20:09:37 +0545

    test

--- components/layout/contact.tsx
+++ components/layout/contact.tsx
@@ -1,28 +1,4 @@
-/*import Image from "next/image";
-export const Contact = () => {
-  return (
-    <div id='contact-us' className="flex  flex-col md:justify-evenly md:flex-row p-4  ">
-      <div className="flex relative  px-10 w-[500px] md:w-[500px] h-[300px]  md:h-[450px] mt-4 ">
-        <Image src={'/map.png'} objectFit="contain" alt={'table'} width="600"
-          height="1000"
-          className=" absolute bg-mint text-mint fill-current border w-full h-full   border-gray-200 "></Image>
-      </div>
 
-      <div className="flex flex-col items-center md:items-start mt-8 md:mt-0   ">
-        <div className="right  w-[500px] md:w-[500px] h-[450px]  flex flex-col  gap-3    " >
-        <h1 className="text-2xl stylefont font-semibold text-black  ">Reach Out. We are Here to Help.</h1>
-          <h1 className="text-4xl font-semibold text-[#7e3939] font1">Contact Us</h1>
-          <input type="text" placeholder="Full Name" className="w-full border-2 px-5 py-2  border-gray-400 font-semibold text-gray-500 "></input>
-          <input type="text" placeholder="Phone Number" className="w-full border-2   px-5 py-2 mt-2 border-gray-400 font-semibold text-gray-500 "></input>
-          <input type="text" placeholder="Email" className="w-full border-2   px-5 py-2 mt-2 border-gray-400 font-semibold text-gray-500"></input>
-          <input type="text" placeholder="Message" className="  border-2 mt-2  px-5 py-10 border-gray-400 font-semibold text-gray-500 "></input>
-          <button className=" text-[#ffff] flex gap-2 items-center p-2 px-4 border w-[120px] h-[50px] mt-3 bg-[#0F172B] "> Send Now </button>
-        </div>
-
-      </div>
-      
-    </div>
-) }*/
     import Image from "next/image";
     import { GoDash } from 'react-icons/go';
     export const Contact = () => {
@@ -35,9 +11,9 @@ export const Contact = () => {
           <div className="flex flex-col items-center md:items-start mt-4 md:mt-0 w-full md:w-[50%] px-4 md:px-10 pt-10">
             <div className="w-full flex flex-col gap-3">
               <h1 className="text-2xl stylefont font-semibold flex -mt-3 text-black"> <div className=' -ml-1 -mt-5 text-7xl'>
-            <GoDash />{' '}
-          </div>{' '}Have a questions or need more information?<div className=' -ml-1 -mt-5 text-7xl'>
-            <GoDash />{' '}
+            <GoDash />
+          </div>Have a questions or need more information?<div className=' -ml-1 -mt-5 text-7xl'>
+            <GoDash />
           </div>{' '}</h1>
           <h1 className="text-2xl stylefont justify-center -mt-6 font-semibold flex text-black"> We'd love to hear from you!</h1>
               <h1 className="text-4xl font-semibold text-[#7e3939] font1">Contact Us</h1>
