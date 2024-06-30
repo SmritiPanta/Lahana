d02be9b0ee45ca10e21a2676362811b4326381f6
Smriti Panta <smritipanta1@gmail.com>
2024-06-30 20:35:33 +0545

    final

--- components/layout/contact.tsx
+++ components/layout/contact.tsx
@@ -10,12 +10,13 @@
     
           <div className="flex flex-col items-center md:items-start mt-4 md:mt-0 w-full md:w-[50%] px-4 md:px-10 pt-10">
             <div className="w-full flex flex-col gap-3">
-              <h1 className="text-2xl stylefont font-semibold flex -mt-3 text-black"> <div className=' -ml-1 -mt-5 text-7xl'>
+              <h1 className="text-2xl stylefont font-semibold flex -mt-3 text-black"> <div className=" -ml-1 -mt-5 text-7xl">
             <GoDash />
-          </div>Have a questions or need more information?<div className=' -ml-1 -mt-5 text-7xl'>
+          </div>Have a questions or need more information?<div className="-ml-1 -mt-5 text-7xl">
             <GoDash />
-          </div>{' '}</h1>
-          <h1 className="text-2xl stylefont justify-center -mt-6 font-semibold flex text-black"> We'd love to hear from you!</h1>
+          </div>
+          </h1>
+          <h1 className="text-2xl stylefont justify-center -mt-6 font-semibold flex text-black"> We would love to hear from you!</h1>
               <h1 className="text-4xl font-semibold text-[#7e3939] font1">Contact Us</h1>
               <input type="text" placeholder="Full Name" className="w-full border-2 px-5 py-2 border-gray-400 font-semibold text-gray-500 rounded-lg"></input>
               <input type="text" placeholder="Phone Number" className="w-full border-2 px-5 py-2 mt-2 border-gray-400 font-semibold text-gray-500 rounded-lg"></input>
