e7d87e9bc9d4ec21a3c4da908ab186edcd40af7a
Smriti Panta <smritipanta1@gmail.com>
2024-07-05 11:28:27 +0545

    adjustmap

--- components/layout/contactus.tsx
+++ components/layout/contactus.tsx
@@ -16,7 +16,7 @@ import Link from "next/link";
         <div  className="flex flex-col md:flex-row ">
           
     
-          <div className="flex flex-col items-center md:items-start  md:mt-0 w-full md:w-[50%] px-4 md:px-10 pt-5">
+          <div className="flex flex-col items-center md:items-start w-[30%] md:mt-0 md:w-[50%] px-4 md:px-10 pt-5">
             <div className="w-full flex flex-col gap-3">
              
           <div className="flex flex-col pt-3">
@@ -50,7 +50,7 @@ import Link from "next/link";
           {/* <div className="flex relative w-full md:w-[50%] h-[320px] md:h-[450px] mt-4 mr-24"> */}
             {/* <Image src={'/map.png'} objectFit="contain" alt={'table'} layout="fill" className="absolute bg-mint text-mint  "></Image> */}
           {/* </div> */}
-          <div className="mb-5 pt-3"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.241857245103!2d85.31672892425418!3d27.709817875384672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1906c7d8ba87%3A0xeb2649ae59dcda2e!2sKamaladi%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1720066090246!5m2!1sen!2snp" width="600" height="430"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe></div>
+          <div className=" w-9/10 md:w-1/2 px-4 md:px-10 pt-5 mb-5 "><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.241857245103!2d85.31672892425418!3d27.709817875384672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1906c7d8ba87%3A0xeb2649ae59dcda2e!2sKamaladi%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1720066090246!5m2!1sen!2snp" width="100%" height="430"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe></div>
         </div>
         </section>
       )
